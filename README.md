ac2804b482876556d1bb9a6e1fb7e78580cf7757
maxbycode <maxbycode@gmail.com>
2017-03-22 08:24:54 -0600

    Adjusted README and secured webhook responses
    Webhook responses now require a secret token validation. Corrected some grammar in README

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ It is not recommended to use unless you want to give feedback this early but if 
 v0lture stats has been built and tested using PHP `7.0` and `7.1`, these are the only two versions we will be supporting with this software.
 
 ### Developing
-A `secrets.template` file should be renamed `secrets.php` and load all of your environment variables in order to use on your own machine.
+A `secrets.template` file should be renamed `secrets.php` and set all of your environment variables in order to use on your own deployment.
 
 If you have any comments, questions or ideas, send an email to contact@v0lture.com
--- receive/github.php
+++ receive/github.php
@@ -1,8 +1,30 @@
 <?php
 
-    // testing
-    $fp = fopen('github-'.microtime(true).'.txt', 'w');
-    fwrite($fp, json_encode($_POST));
-    fclose($fp);
+    if(isset($_POST)) {
+
+        // load variables
+        require_once "../secrets.php";
+
+        // validate secret
+        $oursecret = "sha1=".hash_hmac("sha1", json_encode($_POST), $webhooksecret);
+        $theirsecret = $_SERVER["HTTP_X_HUB_SIGNATURE"];
+
+        if(hash_equals($theirsecret, $oursecret)) {
+            http_response_code(401);
+            echo "Invalid secret!<br />Make sure your secret in Stats and secret in your webhook are the same.<br />This incident has been reported.";
+            // todo: connect to vucket and report security incident
+        } else {
+            // passed hash check
+            $fp = fopen('github-'.microtime(true).'.txt', 'w');
+            fwrite($fp, json_encode($_POST));
+            fclose($fp);
+
+            echo "Your yummy data has been received by Dvorak.";
+        }
+
+    } else {
+        // No post data - go to github stats
+        header("Location: https://stats.v0lture.com/#github");
+    }
 
 ?>
